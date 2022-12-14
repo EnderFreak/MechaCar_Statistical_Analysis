3f66994ad782b1e8c139774b704813953bbb15b8
EnderFreak <vkj1804@gmail.com>
2022-12-14 15:00:31 -0500

    Adding part 2 in Rscript and images folder with images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,21 @@ mecha_model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground
 
 #6 - find p-value and r-squared val for the linear regression
 summary(mecha_model)
+
+
+#PART 2 : CREATE VISUALIZATIONS FOR THE TRIP ANALYSIS
+
+#2 - import csv file as a table
+coil_table <- read.csv(file = "Suspension_Coil.csv", check.names=F, stringsAsFactors = F)
+
+#3 - create summary df to get measures of central tendency for the PSI metrics
+total_summary <- coil_table %>% summarize(Mean_PSI = mean(PSI),
+                               Median_PSI = median(PSI),
+                               Variance = var(PSI),
+                               Std_Dev = sd(PSI))
+
+#4 - create summary df per lot with central measures of tendency
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI = mean(PSI),
+                                                                       Median_PSI = median(PSI),
+                                                                       Variance = var(PSI),
+                                                                       Std_Dev = sd(PSI))
