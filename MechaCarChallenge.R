d390d8159c258fddf75994b04adf7309ded78c4c
EnderFreak <vkj1804@gmail.com>
2022-12-14 16:59:30 -0500

    Adding part 3 in Rscript and images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -30,3 +30,39 @@ lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI
                                                                        Median_PSI = median(PSI),
                                                                        Variance = var(PSI),
                                                                        Std_Dev = sd(PSI))
+
+#PART 3 : T-TESTS ON SUSPENSION COILS
+
+#1 - t-test if psi across ALL manuf. lots is statistically different from pop. mean of 1,500lbs per square inch
+#test for normality first
+shapiro.test(coil_table$PSI)
+
+t.test(log10(coil_table[["PSI"]]), mu=mean(log10(1500)))
+
+#2 - 3 more t-tests per lot, using subset to group lots, to see if statistically different from pop. mean
+#create subsets to store each lot in vars for easier reading
+coil_lot1 <- subset(coil_table, Manufacturing_Lot == "Lot1")
+coil_lot2 <- subset(coil_table, Manufacturing_Lot == "Lot2")
+coil_lot3 <- subset(coil_table, Manufacturing_Lot == "Lot3")
+
+#test for normality before t-tests
+shapiro.test(coil_lot1$PSI)
+shapiro.test(coil_lot2$PSI)
+shapiro.test(coil_lot3$PSI)
+
+t.test(log10(coil_lot1[["PSI"]]), mu=mean(log10(1500)))
+t.test(log10(coil_lot2[["PSI"]]), mu=mean(log10(1500)))
+t.test(log10(coil_lot3[["PSI"]]), mu=mean(log10(1500)))
+
+
+
+
+
+
+
+
+
+
+
+
+
